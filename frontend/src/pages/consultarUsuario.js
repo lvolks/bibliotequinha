78687ceb193db15a6ffe55d4191d88b9d48cbd22
Lukas Volks <professionalvolks@gmail.com>
2023-06-28 02:45:24 -0300

    Consulta e edição de usuário

---
+++ frontend/src/pages/consultarUsuario.js
@@ -0,0 +1,10 @@
+import ConsultarUsuario from '../components/Useredit/ConsultarUsuario';
+
+export default function consultarUsuario() {
+
+  return <div className="container detalhes-container">
+ 
+    <h4>Consultar Usuário</h4>
+    <ConsultarUsuario />
+  </div>
+}
---
+++ frontend/src/pages/editarUsuario.js
@@ -0,0 +1,10 @@
+import EditarUsuario from '../components/Useredit/EditarUsuario';
+
+export default function editarUsuario() {
+
+  return <div className="container detalhes-container">
+ 
+    <h4>Editar Usuário</h4>
+    <EditarUsuario />
+  </div>
+}
