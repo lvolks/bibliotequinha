043910f59ce009f435ad20d9c0662895368ee9df
Rodolfo <rodolfo.b.liberado@gmail.com>
2023-06-28 16:24:27 -0300

    Alterações no design das páginas home, login e cadastro

--- frontend/src/pages/Admin.js
+++ frontend/src/pages/Admin.js
@@ -1,55 +1,61 @@
-import { useState, useEffect } from 'react';
-import { useParams } from 'react-router-dom';
+import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
 import api from "../services/api";
-import CadastroLivro from '../components/Admin/CadastroLivro';
-import CadastroGenero from '../components/Admin/CadastroGenero';
-import ConsultaReserva from '../components/Admin/ConsultaReserva';
+import CadastroLivro from "../components/Admin/CadastroLivro";
+import CadastroGenero from "../components/Admin/CadastroGenero";
+import ConsultaReserva from "../components/Admin/ConsultaReserva";
+import "./admin.css";
 
 export default function Admin() {
   async function load() {
     try {
-      const token = localStorage.getItem("token")
+      const token = localStorage.getItem("token");
 
       if (!token) {
-        window.location.href = '/login'
+        window.location.href = "/login";
       }
 
-      const user = await api.get('/auth/me', {
+      const user = await api.get("/auth/me", {
         headers: {
-          authorization: `Bearer ${token}`
-        }
-      })
+          authorization: `Bearer ${token}`,
+        },
+      });
 
       if (!user.data.funcionario) {
-        alert('Você não tem acesso!')
-        window.location.href = '/login'
-      } 
+        alert("Você não tem acesso!");
+        window.location.href = "/login";
+      }
     } catch (error) {
-      console.error(error)
-      window.location.href = '/login'
-
+      console.error(error);
+      window.location.href = "/login";
     }
   }
-  
+
   useEffect(() => {
-   load()
-  })
-  return <div className="container detalhes-container">
-    <h2>Painel de Admin</h2>
+    load();
+  });
+  return (
+    <div className="container">
+      <h2>Painel de Admin</h2>
+      <div className="container-2">
+        <div>
+          <h4>Cadastrar livro</h4>
 
-    
-    <h4>Cadastrar livro</h4>
+          <CadastroLivro />
+        </div>
+        <div>
+          <h4>Cadastrar genero</h4>
+          <br />
+          <CadastroGenero />
+        </div>
+      </div>
+      <h4>Consultar reserva</h4>
+      <br />
+      <ConsultaReserva />
 
-    <CadastroLivro />
-    <br/>
-    <h4>Cadastrar genero</h4>
-    <br />
-    <CadastroGenero />
-    <br/>
-    <h4>Consultar reserva</h4>
-    <br />
-    <ConsultaReserva />
-
-    <br /><br /><br />
-  </div>
+      <br />
+      <br />
+      <br />
+    </div>
+  );
 }
